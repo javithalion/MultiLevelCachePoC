bb5d82880df6b37cd58d7a125979b54e476b7822
Javithalion <javier_cervera_cano@hotmail.com>
2015-11-19 14:21:44 +0100

    Local cache application simplified

--- MultiLevelCachePoC.ConsoleLocalCache/Program.cs
+++ MultiLevelCachePoC.ConsoleLocalCache/Program.cs
@@ -18,21 +18,15 @@ namespace MultiLevelCachePoC.ConsoleLocalCache
             ICacheManager cacheManager = new CacheManager("LocalCache", persistenceEngine, masterCache);
 
             var engine_A = new Engine() { Id = 1, Description = "This is the engine A" };
-            var station_A = new Station() { Id = 1, Description = "This is the station A" };
 
-            cacheManager.Set(engine_A, SyncMode.Sync);
-            cacheManager.Set(station_A);
-
+            cacheManager.Set(engine_A, SyncMode.SyncParent);
             var getResult_1 = cacheManager.Get(engine_A.GetUniqueHash());
-            var getResult_2 = cacheManager.Get(station_A.GetUniqueHash());
 
             cacheManager.Delete(engine_A.GetUniqueHash());
             var getResult_3 = cacheManager.Get(engine_A.GetUniqueHash());
-            var getResult_4 = cacheManager.Get(station_A.GetUniqueHash());
 
-            var getResult_5 = cacheManager.Get(engine_A.GetUniqueHash(), SyncMode.Sync);
+            var getResult_5 = cacheManager.Get(engine_A.GetUniqueHash(), SyncMode.SyncParent);
             var getResult_6 = cacheManager.Get(engine_A.GetUniqueHash());
-
         }
     }
 }
